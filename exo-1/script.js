da079e24dc035405592267455e37af0901f40d0e
Ismail.rahaoui <ismail.rahaoui@le-campus-numerique.fr>
2022-10-06 11:32:07 +0200

    Exo 1

--- exo-1/script.js
+++ exo-1/script.js
@@ -1,2 +1,11 @@
 // you can write js here
 console.log('hello from file');
+
+// var kelvins = 294;
+var kelvins = prompt("Quelle est la température en Kelvins aujourd'hui ?");
+var celsius = kelvins - 273; // je crée une varaible celsius qui a la valeur de kelvins (294) - 273 = 21
+var Fahrenheits = celsius * (9/5) + 32 // je crée une variable qui se nomme Fahrenheits qui vaut celius * (9/5) + 32
+
+console.log(kelvins);
+console.log(celsius);
+console.log(Math.floor(Fahrenheits));
